9dd45f731c31a3e1059e4daf03cf26a9cacadd87
Mateusz Kroplewski <kroplewskimateusz@gmail.com>
2023-11-13 19:42:48 +0000

    started working on dashboard

--- ProjectFlow.Web/Controllers/HomeController.cs
+++ ProjectFlow.Web/Controllers/HomeController.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using ProjectFlow.Web.Models;
 using System.Diagnostics;
 
 namespace ProjectFlow.Web.Controllers
@@ -18,15 +17,5 @@ namespace ProjectFlow.Web.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
 }
